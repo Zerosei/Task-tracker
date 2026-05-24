87650b72ec02a7cfd11de0843f61dd45e28e8030
Fernando Sánchez <zerosei.dev@gmail.com>
2026-05-24 11:10:41 +0200

    Initialize task tracker application with core functionality for adding tasks and managing a JSON file for storage

---
+++ app.js
@@ -0,0 +1,38 @@
+// Añadir, actualizar y eliminar tareas
+// Marcar una tarea como en progreso o completada
+// Listar todas las tareas
+// Listar todas las tareas completadas
+// Listar todas las tareas pendientes
+// Listar todas las tareas en progreso
+
+// Restricciones a tener en cuenta:
+
+// Puedes usar cualquier lenguaje de programación.
+// Usa argumentos posicionales en la línea de comandos para recibir la entrada del usuario.
+// Usa un archivo JSON para almacenar las tareas en el directorio actual.
+// El archivo JSON debe crearse si no existe.
+// Usa el módulo nativo de sistema de archivos de tu lenguaje para interactuar con el JSON.
+// No uses librerías o frameworks externos.
+// Maneja los errores y casos extremos de forma adecuada.
+import {addTask} from "addTask.js"
+
+
+import readline from 'readline/promises';
+const fs = require('fs')
+
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+
+while(true) {
+    if (!fs.existsSync('tasks.json')) {
+        fs.writeFileSync('tasks.json', JSON.stringify([], null, 2));
+    }
+    const entrada = await rl.question('Task Tracker: ')
+
+    if (entrada === "add"){
+        addTask("Hola")
+    }
+    if (entrada === "exit"){
+        rl.close();
+        break
+    }
+}
---
+++ functions/addTask.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+
+function addTask(description){
+    // Accedemos al JSON de tareas
+    const tasks = JSON.parse(fs.readFileSync('tasks.json', 'utf8'));
+    // Para el id de nueva tarea medimos la longitud
+    const id = tasks.length;
+    // Creamos un objeto de nueva tarea que pushearemos al json 
+    const newTask = {id: id, description: description, status: "todo", createdAt: Date.now().toLocaleDateString, updatedAt: Date.now().toLocaleDateString};
+    tasks.push(newTask)
+    // Guardamos en el json
+    fs.writeFileSync("tasks.json", JSON.stringify(tasks, null, 2))
+}
